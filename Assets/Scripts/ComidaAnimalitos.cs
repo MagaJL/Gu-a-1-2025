08d425faec585e8294768b4587a89db1d4c13368
MagaJL <48716048@est.ort.edu.ar>
2025-04-07 10:41:44 -0300

    Tema 3: Punto 1 y 2 listos

---
+++ Assets/Scripts/ComidaAnimalitos.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComidaAnimalitos : MonoBehaviour
+{
+    public string codigo;
+    public int diasQueSeQueda;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (codigo == "G" && diasQueSeQueda > 3)
+        {
+            
+        }
+        else if (codigo == "PP" && diasQueSeQueda > 3)
+        {
+
+        }  
+        else if (codigo == "PG" && diasQueSeQueda > 3)
+        {
+
+        }
+        else
+        {
+            Debug.Log("El codigo y/o días de estadia no es/son valido/s");
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
